932ea1b490a1c417da2db693fc4660ade2bfc137
le-scientifique <sreeharsharamesh@gmail.com>
2016-06-18 11:46:02 -0400

    looks like login is ok!

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,11 +6,20 @@ exports.login = function(req, res) {
   console.log(req.body.password)
   db.fn_get_user(req.body.user,req.body.password,function(err, results) {
     if(err) { 
-        res.send(500,"Server Error"); 
+	console.log(err)
+        res.send({"status":500,"message":"Server Error"}); 
         return;
     }
-    // Respond with results as JSON
-    res.send(results);
+    
+   else if(results!=undefined && results.length > 0){
+    result = results[0]
+    var obj = {"verified":true, "name":result.user_login_id, "credits":result.user_credits}
+    res.status(200).json(obj);
+    }
+   else {
+	res.status(200).send({"message":"User not found!"});
+        return;
+    }
   });
 };
 
